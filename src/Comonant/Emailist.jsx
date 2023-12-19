8f714fdce3f5620aead9de84853c0f877dae4507
Jshubham986 <jshubham986@gmail.com>
2023-12-19 15:30:31 +0530

    New_Email

--- src/Comonant/Emailist.jsx
+++ src/Comonant/Emailist.jsx
@@ -1,18 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Email_settings from './Email_settings'
 import Emailtype from './Emailtype'
 import Emailbody from './Emailbody'
+import db from '../firebase'
+
 
 function Emailist() {
+
+  const [email, setEmail] = useState([])
+  useEffect(() => {
+    db.collection("Emails").onSnapshot(snapshot => {
+      setEmail(snapshot.docs.map((doc => ({
+        id: doc.id,
+        data: doc.data(),
+
+      }))))
+        .collection("Emails")
+        .orderBy("", "desc")
+    })
+  }, [])
+  console.log(email)
   return (
     <div className='email'>
-        <Email_settings/>
-        <Emailtype/>
-        <Emailbody
-        name="Shubham Jadhav"
-        subject="This is subject"
-        message="We are in message section.We are in message section.We are in message section.We are in message section.We are in message section.We are in message section.We are in message section.We are in message section.We are in message section."
-        time="3:00 PM"/>
+      <Email_settings />
+      <Emailtype />
+
+      {
+        email.map(({ id, data }) => {
+          return (
+            <Emailbody
+              ket={id}
+              name={data.to}
+              subject={data.subject}
+              message={data.message}
+              time={new Date(data.createdAt?.seconds * 1000).toLocaleTimeString()}
+            />
+          )
+        })
+      }
+
+
     </div>
   )
 }
